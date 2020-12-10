ab2c7d3909ceff646eaba0d749fb8ded811d9296
Daulet Zhanguzin <daulet@zhanguzin.kz>
2020-12-10 21:08:07 +0000

    fix train_efficientnet example

--- examples/train_efficientnet.py
+++ examples/train_efficientnet.py
@@ -90,8 +90,7 @@ if __name__ == "__main__":
     t.set_description("loss %.2f accuracy %.2f -- %.2f + %.2f + %.2f + %.2f = %.2f -- %d" %
       (loss, accuracy,
       fp_time, bp_time, opt_time, finish_time,
-      fp_time + bp_time + opt_time + finish_time,
-      Tensor.allocated))
+      fp_time + bp_time + opt_time + finish_time))
 
     del out, y, loss
 
