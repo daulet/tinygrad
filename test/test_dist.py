ebd6221e5fbb8182c9e9d4a434f0e3577957f938
Daulet Zhanguzin <daulet@zhanguzin.kz>
2020-12-28 14:39:00 +0000

    Add separate forward and backward tests

--- test/test_dist.py
+++ test/test_dist.py
@@ -1,12 +1,13 @@
 from multiprocessing import Process
 import numpy as np
+import os
 import time
 import torch
 import torch.distributed.rpc as rpc
 import unittest
 
 from distributed.rpc import Worker, RPC
-from tinygrad.tensor import Tensor, GPU
+from tinygrad.tensor import Device, Tensor, GPU
 
 
 x_init = np.random.randn(3,3).astype(np.float32)
@@ -38,48 +39,36 @@ def compute_torch(t1, t2):
 
 
 class TestTinygrad(unittest.TestCase):
-  gpu = False
+  device = Device.CPU
+  port = 65432
 
-  def test_distributed_grad(self):
+  def test_distributed_forward(self):
+
     def test_tinygrad():
-        """
-        TODO
-        assume the other agent is running exactly the same model,
-        so can ref func by ID instead of trying to serialize it
-        compare (timewise) tinygrad distributed over CPU and GPU vs pure pytorch on CPU
-        """
-
         # Arrange
-
-        port = 65432
-        proc = Process(target=run_remote_tinygrad, args=[port])
+        proc = Process(target=run_remote_tinygrad, args=[self.port])
         proc.start()
         time.sleep(0.1)
 
         # Act
-
-        t1 = Tensor(x_init, gpu=self.gpu)
-        t2 = Tensor(y_init, gpu=self.gpu)
+        t1 = Tensor(x_init, device=self.device)
+        t2 = Tensor(y_init, device=self.device)
         t3 = RPC.send(compute_tinygrad, t1, t2)
-        t4 = Tensor(z_init, gpu=self.gpu)
-        res = t3.mul(t4)
+        t4 = Tensor(z_init, device=self.device)
+        t5 = t3.mul(t4)
 
         # clean up
-
         RPC.done()
         proc.join()
 
-        return res.cpu().data
+        return t5.cpu().data
 
 
     def test_pytorch():
-
       # Arrange
-
       world_size = 2
       proc = Process(target=run_remote_torch, args=[1, world_size])
       proc.start()
-
       rpc.init_rpc(
         name="worker0",
         rank=0,
@@ -87,30 +76,95 @@ class TestTinygrad(unittest.TestCase):
       )
 
       # Act
-
       t1 = torch.tensor(x_init, requires_grad=True)
       t2 = torch.tensor(y_init, requires_grad=True)
       t3 = rpc.rpc_sync("worker1", compute_torch, args=(t1, t2))
       t4 = torch.tensor(z_init, requires_grad=True)
-      res = torch.mul(t3, t4)
+      t5 = torch.mul(t3, t4)
 
       # clean up
-
       rpc.shutdown()
       proc.join()
 
-      return res.detach().numpy()
-
+      return t5.detach().numpy()
 
     # Assert
-
     for x,y in zip(test_tinygrad(), test_pytorch()):
       np.testing.assert_allclose(x, y, atol=1e-5)
 
 
+  def test_distributed_backward(self):
+
+    def test_tinygrad():
+        # Arrange
+        proc = Process(target=run_remote_tinygrad, args=[self.port])
+        proc.start()
+        time.sleep(0.1)
+
+        # Act
+        t1 = Tensor(x_init, device=self.device)
+        t2 = Tensor(y_init, device=self.device)
+        t3 = RPC.send(compute_tinygrad, t1, t2)
+        t4 = Tensor(z_init, device=self.device)
+        t5 = t3.mul(t4)
+        loss = t5.sum()
+        loss.backward()
+
+        # clean up
+        RPC.done()
+        proc.join()
+
+        return loss.cpu().data, t1.grad.cpu().data, t2.grad.cpu().data, \
+          t3.grad.cpu().data, t4.grad.cpu().data, t5.grad.cpu().data
+
+
+    def test_pytorch():
+      # Arrange
+      world_size = 2
+      proc = Process(target=run_remote_torch, args=[1, world_size])
+      proc.start()
+      rpc.init_rpc(
+        name="worker0",
+        rank=0,
+        world_size=world_size
+      )
+
+      # Act
+      t1 = torch.tensor(x_init, requires_grad=True)
+      t2 = torch.tensor(y_init, requires_grad=True)
+      t3 = rpc.rpc_sync("worker1", compute_torch, args=(t1, t2))
+      t4 = torch.tensor(z_init, requires_grad=True)
+      t5 = torch.mul(t3, t4)
+      loss = t5.sum()
+      loss.backward()
+
+      # clean up
+      rpc.shutdown()
+      proc.join()
+
+      return loss.detach().numpy(), t1.grad, t2.grad, \
+        t3.grad, t4.grad, t5.grad
+
+
+    # Assert
+    for x,y in zip(test_tinygrad(), test_pytorch()):
+      np.testing.assert_allclose(x, y, atol=1e-5)
+
+
+  def setUp(self):
+    # required by torch.rpc.init()
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = str(self.port)
+
+
+  def tearDown(self):
+    del os.environ['MASTER_ADDR']
+    del os.environ['MASTER_PORT']
+
+
 @unittest.skipUnless(GPU, "Requires GPU")
 class TestTinygradGPU(TestTinygrad):
-  gpu = True
+  device = Device.GPU
 
 
 if __name__ == '__main__':
